4ac5d31bfefaae16e85c38639c562b31ea44f4f8
Petr Viktorin <pviktori@redhat.com>
2014-02-13 19:47:31 +0100

    Add a commit reporting tool

---
+++ commit-report.py
@@ -0,0 +1,459 @@
+#!/usr/bin/python3
+
+"""Generate a commit report
+
+Usage:
+  commit-report.py [options] [-v...] [-c KEY=VAL...]
+
+Options:
+  --since=WHEN            Start date [default: one.week.ago]
+  --until=WHEN            End commit
+  --from=COMMIT           Starting commit
+  --to=COMMIT             Ending commit (defaults to {remote}/master)
+  -h, --help              Display this help and exit
+  -v, --verbose           Increase verbosity
+  --config FILE           Configuration file [default: ~/.ipa/pushpatch.yaml]
+  -c, --set-conf=KEY=VAL  Set a config option(s). VAL is in YAML format.
+  --no-trac               Do not contact Trac
+  --no-fetch              Do not synchronize before reporting
+  --mailto=EMAIL          Mail results to this address
+  --mailfrom=EMAIL        Mail results from this address
+  --color=(auto|always|never)  Colorize output [default: auto]
+
+Generate a commit report.
+
+The --mailto and --mailfrom options must be specified together.
+
+Configuration can be specified in the file given by --config.
+Here is an example
+  project-name: FreeIPA
+  clean-repo-path: ~/dev/freeipa-clean
+  ticket-url: https://fedorahosted.org/freeipa/ticket/
+  commit-url: https://fedorahosted.org/freeipa/changeset/
+  bugzilla-bug-url: https://bugzilla.redhat.com/show_bug.cgi?id=
+  trac-xmlrpc-url: https://fedorahosted.org/freeipa/xmlrpc
+  remote: origin
+  smtp-host: smtp.example.com
+  smtp-port: 25
+"""
+
+# TODO: Much of this is copied from pushpatches.py. Make a common library.
+
+import os
+import sys
+import subprocess
+import re
+import collections
+import pprint
+import xmlrpc.client
+import functools
+import datetime
+import smtplib
+from email.mime.text import MIMEText
+
+import docopt     # yum install python3-docopt
+import yaml       # yum install python3-PyYAML
+import blessings  # yum install python3-blessings
+import pytz       # yum install python3-pytz
+
+COLOR_OPT_MAP = {'auto': False, 'always': True, 'never': None}
+
+SubprocessResult = collections.namedtuple(
+    'SubprocessResult', 'stdout stderr returncode')
+TracTicketData = collections.namedtuple(
+    'TracTicketData', 'id time_created time_changed attributes')
+CommitGroup = collections.namedtuple(
+    'CommitGroup', 'info commits')
+AuthorInfo = collections.namedtuple(
+    'AuthorInfo', 'name email date')
+
+
+class reify(object):
+    # https://github.com/Pylons/pyramid/blob/1.4-branch/pyramid/decorator.py
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+        self.__doc__ = getattr(wrapped, '__doc__', None)
+
+    def __get__(self, inst, objtype=None):
+        if inst is None:
+            return self
+        val = self.wrapped(inst)
+        setattr(inst, self.wrapped.__name__, val)
+        return val
+
+
+@functools.total_ordering
+class Ticket(object):
+    """Trac ticket with lazily fetched information"""
+    _memo = {}
+
+    def __init__(self, cli, number):
+        self.cli = cli
+        self.number = number
+
+    @reify
+    def data(self):
+        try:
+            return self._memo[self]
+        except KeyError:
+            self.cli.eprint('Retrieving ticket %s' % self.number)
+            data = TracTicketData(*self.cli.trac.ticket.get(self.number))
+            self._memo[self] = self
+            return data
+
+    @property
+    def attributes(self):
+        return self.data.attributes
+
+    @property
+    def url(self):
+        return self.cli.config['ticket-url'] + str(self.number)
+
+    def __hash__(self):
+        return hash(self.number) ^ hash(self.cli)
+
+    def __eq__(self, other):
+        return self.number == other.number
+
+    def __lt__(self, other):
+        return self.number < other.number
+
+
+class CLIHelper(object):
+    def __init__(self, options):
+        self.output = []
+        self.options = options
+        with open(os.path.expanduser(options['--config'])) as conf_file:
+            self.config = yaml.safe_load(conf_file)
+        for opt in options['--set-conf']:
+            key, sep, value = opt.partition('=')
+            self.config[key] = yaml.safe_load(value)
+        self.term = blessings.Terminal(
+            force_styling=COLOR_OPT_MAP[options['--color']])
+        self.verbosity = self.options['--verbose']
+        if self.verbosity:
+            self.eprint('Options:')
+            self.eprint(pprint.pformat(self.options))
+            self.eprint('Config:')
+            self.eprint(pprint.pformat(self.config))
+        if self.options['--no-trac']:
+            self.trac = None
+        else:
+            url = self.config['trac-xmlrpc-url']
+            self.trac = xmlrpc.client.ServerProxy(url)
+
+        self.color_arg = self.options['--color']
+        if self.color_arg == 'auto':
+            if self.term.is_a_tty:
+                self.color_arg = 'always'
+            else:
+                self.color_arg = 'never'
+
+    def die(self, message):
+        self.eprint(self.term.red(message), file=sys.stderr)
+        exit(1)
+
+    def eprint(self, *messages, **kwargs):
+        """Print to stderr"""
+        kwargs.setdefault('file', sys.stderr)
+        self.print(*messages, **kwargs)
+
+    def print(self, *objects, sep=' ', end='\n', file=sys.stdout):
+        if file == sys.stdout:
+            self.output.append(sep.join(str(o) for o in objects) + end)
+        print(*objects, sep=sep, end=end, file=file)
+
+    def runcommand(self, argv, *, check_stdout=None, check_stderr=None,
+                   check_returncode=0, stdin_string='', fail_message=None,
+                   timeout=5, verbosity=None):
+        """Run a command in a subprocess, check & return result"""
+        argv_repr = ' '.join(self.shell_quote(a) for a in argv)
+        if verbosity is None:
+           verbosity = self.verbosity 
+        if verbosity:
+            self.eprint('+', self.term.blue(argv_repr))
+        if verbosity > 2:
+            self.eprint(self.term.yellow(stdin_string.rstrip()))
+        PIPE = subprocess.PIPE
+        proc = subprocess.Popen(argv, stdout=PIPE, stderr=PIPE, stdin=PIPE)
+        try:
+            stdout, stderr = proc.communicate(stdin_string.encode('utf-8'),
+                                             timeout=timeout)
+            timeout_expired = False
+        except subprocess.TimeoutExpired:
+            proc.kill()
+            stdout = stderr = b''
+            timeout_expired = True
+        stdout = stdout.decode('utf-8')
+        stderr = stderr.decode('utf-8')
+        returncode = proc.returncode
+        failed = any([
+            timeout_expired,
+            (check_stdout is not None and check_stdout != stdout),
+            (check_stderr is not None and check_stderr != stderr),
+            (check_returncode is not None and check_returncode != returncode),
+        ])
+        if failed and not verbosity:
+            self.eprint('+', self.term.blue(argv_repr))
+        if failed or verbosity >= 2:
+            if stdout:
+                self.eprint(stdout.rstrip())
+            if stderr:
+                self.eprint(self.term.yellow(stderr.rstrip()))
+            self.eprint('→ %s' % self.term.blue(str(proc.returncode)))
+        if failed:
+            if timeout_expired: 
+                self.die('Command timeout expired')
+            elif fail_message:
+                self.die(fail_message)
+            else:
+                self.die('Command failed')
+        return SubprocessResult(stdout, stderr, returncode)
+
+    @staticmethod
+    def shell_quote(arg):
+        """Quote an argument for the shell"""
+        if re.match('^[-_.:/=a-zA-Z0-9]*$', arg):
+            return arg
+        else:
+            return "'%s'" % arg.replace("'", r"'\''")
+
+    @staticmethod
+    def cleanpath(path):
+        """Return absolute path with leading ~ expanded"""
+        path = os.path.expanduser(path)
+        path = os.path.abspath(path)
+        return path
+
+    def pprint(text, language=None):
+        'TODO'
+
+
+def groupby(commits, get_groupkeys,
+            get_sortkey=lambda group: (-group.info['num_commits'],
+                                       -group.info['changed'])):
+    groups = {}
+    for commit in commits:
+        for key in get_groupkeys(commit):
+            group = groups.setdefault(key, CommitGroup({}, []))
+            group.commits.append(commit)
+    for key, group in groups.items():
+        group.info['key'] = key
+        group.info['files'] = {}
+        group.info['added'] = 0
+        group.info['removed'] = 0
+        group.info['tickets'] = set()
+        group.info['num_commits'] = len(group.commits)
+        for commit in group.commits:
+            for filename, (added, removed) in commit.files.items():
+                add1, rem1 = group.info['files'].get(filename, (0, 0))
+                add1 += added
+                rem1 += removed
+                group.info['added'] += added
+                group.info['removed'] += removed
+                group.info['files'][filename] = add1, rem1
+            group.info['tickets'].update(commit.tickets)
+        group.info['changed'] = group.info['added'] + group.info['removed']
+    return sorted(groups.values(), key=get_sortkey)
+
+
+class Commit(object):
+    def __init__(self, sha1):
+        self.sha1 = sha1
+        self.message = []
+        self.parents = []
+        self.tickets = set()
+        self.files = {}
+        self.reviewers = []
+
+    @staticmethod
+    def parse_commit_info(line):
+        match = re.match(r'(.+) <(.+@.+)> (\d+) ([+-])(\d\d)(\d\d)', line)
+        offset = 60 * int(match.group(5)) + int(match.group(6))
+        if match.group(4) == '-':
+            offset = -offset
+        date = datetime.datetime.fromtimestamp(int(match.group(3)),
+                                               pytz.FixedOffset(offset))
+        return AuthorInfo(match.group(1), match.group(2), date)
+
+    @property
+    def reviewers_short(self):
+        return ','.join(shorten_author(r) for r in self.reviewers)
+
+    @property
+    def author(self):
+        return '%s <%s>' % (self.author_info.name, self.author_info.email)
+
+    @property
+    def commit_date(self):
+        return self.committer_info.date
+
+    @property
+    def author_short(self):
+        return shorten_author(self.author)
+
+    @property
+    def added(self):
+        return sum(a for a, r in self.files.values())
+
+    @property
+    def removed(self):
+        return sum(r for a, r in self.files.values())
+
+
+def shorten_author(author):
+    match = re.match(r'^.*<(.*)@.*> *$', author)
+    if match:
+        return match.group(1)
+    else:
+        return author
+
+
+def git_date(cli, spec):
+    output = cli.runcommand(['git', 'rev-parse', '--since=%s' % spec]).stdout.strip()
+    name, sep, seconds = output.partition('=')
+    return datetime.datetime.fromtimestamp(int(seconds))
+
+
+def git_describe(cli, commit):
+    argv = ['git', 'describe', commit]
+    return cli.runcommand(argv).stdout.strip()
+
+
+def run(cli):
+    cli.print('Generated:', datetime.datetime.now())
+
+    os.chdir(cli.cleanpath(cli.config['clean-repo-path']))
+
+    remote = cli.config['remote']
+    if not cli.options['--no-fetch']:
+        cli.eprint('Fetching...')
+        cli.runcommand(['git', 'fetch', remote], timeout=60)
+
+    log_args = []
+    end_commit = cli.options['--to'] or '%s/master' % remote
+    start_commit = cli.options['--from']
+    if start_commit:
+        log_args.append('%s..%s' % (start_commit, end_commit))
+        cli.print('From:     ', start_commit,
+              '(%s)' % git_describe(cli, start_commit))
+    else:
+        log_args.append(end_commit)
+    cli.print('To:       ', end_commit, '(%s)' % git_describe(cli, end_commit))
+
+    since = cli.options['--since']
+    if since:
+        log_args.append('--since=%s' % since)
+        cli.print('Since:    ', git_date(cli, since))
+    until = cli.options['--until']
+    if until:
+        log_args.append('--until=%s' % until)
+        cli.print('Until:    ', git_date(cli, until))
+    cli.print()
+
+    static_argv = ['git', 'log', '--boundary', '--format=raw', '--numstat',
+                   '--use-mailmap']
+    output = cli.runcommand(static_argv + log_args).stdout
+
+    ticket_re = re.compile(re.escape(cli.config['ticket-url']) + '(\d+)')
+
+    all_tickets = set()
+    commits = []
+    boundary_commits = []
+
+    for line in output.splitlines():
+        header, sep, content = line.partition(' ')
+        content = content.strip()
+        if not line:
+            pass
+        elif header == 'commit':
+            current_commit = Commit(content.strip(' -'))
+            if content.startswith('-'):
+                boundary_commits.append(current_commit)
+            else:
+                commits.append(current_commit)
+        elif header == 'tree':
+            current_commit.tree = content
+        elif header == 'parent':
+            current_commit.parents.append(content)
+        elif header == 'author':
+            current_commit.author_info = Commit.parse_commit_info(content)
+        elif header == 'committer':
+            current_commit.committer_info = Commit.parse_commit_info(content)
+        elif line.startswith('    '):
+            current_commit.message.append(line[4:].rstrip('\n'))
+            for match in ticket_re.finditer(line):
+                ticket = Ticket(cli, int(match.group(1)))
+                all_tickets.add(ticket)
+                current_commit.tickets.add(ticket)
+            mheader, sep, mcontent = line.strip().partition(' ')
+            if mheader.lower() == 'reviewed-by:':
+                cmd = cli.runcommand(['git', 'check-mailmap', mcontent],
+                                     check_returncode=None)
+                if cmd.returncode == 0:
+                    current_commit.reviewers.append(cmd.stdout.strip())
+                else:
+                    current_commit.reviewers.append(mcontent + ' <bad-entry@invalid>')
+        else:
+            added, removed, filename = line.split('\t')
+            current_commit.files[filename] = int(added), int(removed)
+
+    cli.print('git log:', ' '.join(cli.shell_quote(arg) for arg in log_args))
+    for commit in commits:
+        cli.print('* {c.sha1:7.7} ({c.commit_date}) [{c.author_short};{c.reviewers_short}] {c.message[0]}'.format(c=commit))
+    for commit in boundary_commits:
+        cli.print('^ {c.sha1:7.7} ({c.commit_date}) [{c.author_short};{c.reviewers_short}] {c.message[0]}'.format(c=commit))
+    cli.print()
+
+    rep = lambda title, key: print_report(cli, commits, title, key)
+
+    rep('By patch author', lambda commit: [commit.author])
+    rep('By reviewer', lambda commit: commit.reviewers)
+
+    if cli.options['--mailto']:
+        msg = MIMEText(''.join(cli.output))
+        if until:
+            subject_date = git_date(cli, until).date()
+        else:
+            subject_date = datetime.date.today()
+        msg['Subject'] = '{project} commit report {date} ({start}..{end})'.format(
+            project=cli.config['project-name'],
+            date=subject_date,
+            start=boundary_commits[0].sha1[:7],
+            end=commits[0].sha1[:7])
+        msg['From'] = cli.options['--mailfrom']
+        to = cli.options['--mailto']
+        msg['To'] = to
+
+        host = cli.config['smtp-host']
+        port = int(cli.config['smtp-port'])
+        cli.eprint('Sending results to {to} via {host}:{port}...'.format(
+            to=to,
+            host=host,
+            port=port))
+        smtp = smtplib.SMTP(host, port)
+        smtp.send_message(msg)
+        smtp.quit()
+        cli.eprint('Sent!')
+
+
+def print_report(cli, commits, title, keyfunc):
+    cli.print('%s:' % title)
+    for group in groupby(commits, keyfunc):
+        cli.print('{g.info[num_commits]:4}  {g.info[key]} (+{g.info[added]} -{g.info[removed]})'.format(g=group))
+        for commit in group.commits:
+            cli.print('        {c.sha1:7.7} [{c.author_short};{c.reviewers_short}] {c.message[0]} (+{c.added} -{c.removed})'.format(url=cli.config['commit-url'], c=commit))
+        for ticket in sorted(group.info['tickets']):
+            if cli.trac:
+                if ticket.attributes['status'] == 'closed':
+                    cli.print('        #{t.number} [{t.attributes[owner]}\'s {t.attributes[component]} {t.attributes[type]}] {t.attributes[summary]}'.format(t=ticket))
+                    if ticket.attributes['rhbz'] and ticket.attributes['rhbz'] != '0':
+                        rhbz = ticket.attributes['rhbz'].split(' ', 1)[-1].strip('[]')
+                        cli.print('          BZ {rhbz}'.format(rhbz=rhbz))
+            else:
+                cli.print('        {t.url}'.format(t=ticket))
+    cli.print()
+
+if __name__ == '__main__':
+    cli = CLIHelper(docopt.docopt(__doc__))
+    run(cli)
