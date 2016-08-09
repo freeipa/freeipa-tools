224f8250eca1b67cdac2b47b23daf15ad825deca
Martin <martin.basti@gmail.com>
2016-08-09 23:25:22 +0200

    github-notifications: allow to choose particular actions

--- github-email-notifications/githubconsumer.py
+++ github-email-notifications/githubconsumer.py
@@ -13,5 +13,30 @@ class GithubConsumer(fedmsg.consumers.FedmsgConsumer):
     def __init__(self, *args, **kw):
         super(GithubConsumer, self).__init__(*args, **kw)
 
-    def consume(self, msg):
+        self.topic_mapping = {
+            'org.fedoraproject.prod.github.issue.comment': self.issue_comment,
+            'org.fedoraproject.prod.github.issue.labeled': self.issue_labeled,
+            'org.fedoraproject.prod.github.pull_request.opened': self.pr_opened,
+            'org.fedoraproject.prod.github.pull_request.closed': self.pr_closed,
+            'org.fedoraproject.prod.github.status': self.status,
+        }
+
+    def issue_comment(self, msg):
         pprint(msg)
+
+    def issue_labeled(self, msg):
+        pass
+
+    def pr_opened(self, msg):
+        pprint(msg)
+
+    def pr_closed(self, msg):
+        pprint(msg)
+
+    def status(self, msg):
+        pass
+
+    def consume(self, msg):
+        method = self.topic_mapping.get(msg.get('topic'))
+        if method:
+            method(msg)
