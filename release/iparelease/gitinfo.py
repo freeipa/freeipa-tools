97cadcb29ed8cd5520c0e12eb35fef8aa1f73b6b
Martin Basti <mbasti@redhat.com>
2017-03-13 20:22:32 +0100

    Add release-notes.py script

---
+++ release/iparelease/gitinfo.py
@@ -0,0 +1,164 @@
+#
+# Copyright (C) 2015
+#
+
+import os
+import re
+import subprocess
+
+COMMIT_RE = r"^commit (\w+)$"
+AUTHOR_RE = r"^Author: (.+) <(.+)>$"
+DATE_RE = r"^Date:[ ]+(.+)$"
+DESCRIPTION_RE = r"^    (.*)$"
+REVIEWER_RE = r"^\s*Reviewed-By: (.+) <(.+)>$"
+TICKET_RE = r"^\s*[\w: ]*\s*https://fedorahosted.org/freeipa/ticket/(\d+)\s*$"
+TICKET_RE2 = r"^\s*[\w: ]*\s*https://pagure.io/freeipa/issue/(\d+)\s*$"
+
+
+class GitCommit(object):
+    def __init__(self, commit='', author='', date='',
+                 summary='', description='', reviewers=None):
+        self.commit = commit
+        self.author = author
+        self.date = date
+        self.summary = summary
+        self.description = description
+        self.reviewers = reviewers or []
+        self.tickets = set()
+
+    def __str__(self):
+        return '\n'.join([
+            'Commit: %s' % self.commit,
+            'Author: %s <%s>' % (self.author.name, self.author.mail),
+            'Date: %s' % self.date,
+            'Summary: %s' % self.summary,
+            'Tickets: %s' % self._tickets()
+        ])
+
+    def to_short(self):
+        return "%s %s %s\t %s" % (
+            self.commit, self.date, self.summary, self._tickets())
+
+    def _tickets(self):
+        tickets = ['#' + t for t in self.tickets]
+        return ', '.join(sorted(list(tickets)))
+
+
+class GitAuthor(object):
+    def __init__(self, name, mail):
+        self.name = name
+        self.mail = mail
+        self.commits = []
+        self.reviews = []
+        self.tickets = set()
+
+    def __str__(self):
+        return (u"%s <%s> %s %s %s" % (
+            self.name, self.mail, len(self.commits), len(self.reviews),
+            len(self.tickets)))
+
+    def __unicode__(self):
+        return self.__str__()
+
+    def stats(self):
+        return u'\n'.join([
+            u'Commits: %s' % len(self.commits),
+            u'Reviews: %s' % len(self.reviews),
+            u'Tickets: %s' % len(self.tickets)
+        ])
+
+
+class GitInfo():
+    def __init__(self, repopath):
+        self.repopath = repopath
+        self.commits = []
+        self.authors = dict()
+
+    def __str__(self):
+        out = [
+            "Commits: %s" % len(self.commits),
+        ]
+        for commit in self.commits:
+            out.append(str(commit))
+            out.append('')
+        return '\n'.join(out)
+
+    def load(self, since):
+        datestr = since.isoformat()
+        cmd = ["git", "log", "--since", datestr]
+        os.chdir(self.repopath)
+        result = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        self.add_commits(result)
+
+    def get_log(self, revision_range):
+        cmd = ["git", "log", revision_range]
+        os.chdir(self.repopath)
+        result = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        return result
+
+    def add_commits(self, git_output):
+        lines = git_output.splitlines()
+        commit = None
+        description = None
+        del self.commits[:]
+        self.authors.clear()
+        for line in lines:
+            commit_hash = re.match(COMMIT_RE, line)
+            if commit_hash:
+                commit = GitCommit()
+                self.commits.append(commit)
+                description = []
+                commit.commit = commit_hash.groups()[0]
+
+            author_g = re.match(AUTHOR_RE, line)
+            if author_g:
+                name = author_g.groups()[0]
+                mail = author_g.groups()[1]
+                author = self.get_add_author(name, mail)
+                commit.author = author
+                author.commits.append(commit)
+
+            date = re.match(DATE_RE, line)
+            if date:
+                commit.date = date.groups()[0]
+
+            description_line = re.match(DESCRIPTION_RE, line)
+            if description_line:
+                description.append(description_line.groups()[0])
+                commit.summary = description[0]
+                commit.description = '\n'.join(description)
+
+        for commit in self.commits:
+            self.parse_description(commit)
+
+    def get_add_author(self, name, mail):
+        author = self.authors.get(mail, None)
+        if not author:
+            author = GitAuthor(name, mail)
+            self.authors[mail] = author
+        return author
+
+    def _get_ticket(self, line, commit):
+        for regex in [TICKET_RE, TICKET_RE2]:
+            ticket_g = re.match(regex, line)
+            if ticket_g:
+                ticket_id = ticket_g.groups()[0]
+                commit.author.tickets.add(ticket_id)
+                commit.tickets.add(ticket_id)
+
+    def parse_description(self, commit):
+
+        lines = commit.description.splitlines()
+        for line in lines:
+            #print line
+            reviewer_g = re.match(REVIEWER_RE, line)
+            if reviewer_g:
+                name = reviewer_g.groups()[0]
+                mail = reviewer_g.groups()[1]
+                author = self.get_add_author(name, mail)
+                commit.reviewers.append(author)
+                author.reviews.append(commit)
+            ticket_g = re.match(TICKET_RE, line)
+            self._get_ticket(line, commit)
+
+        return commit
---
+++ release/release-notes.py
@@ -0,0 +1,260 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (C) 2016
+#
+
+import sys
+import argparse
+import traceback
+from libpagure import Pagure
+
+from iparelease.gitinfo import GitInfo
+
+WIKI_BLOB = """
+{{ReleaseDate|%(release_date)s}}
+The FreeIPA team would like to announce FreeIPA %(version)s release!
+
+It can be downloaded from http://www.freeipa.org/page/Downloads. Builds for
+Fedora 24 will be available in the official
+[https://copr.fedorainfracloud.org/coprs/g/freeipa/freeipa-4-4/ COPR repository].
+
+== Highlights in %(version)s ==
+
+'''TODO RELEASE NOTES - put release notes (if any) to proper categories'''
+%(release_notes)s
+'''END TODO'''
+
+=== Enhancements ===
+=== Known Issues ===
+
+=== Bug fixes ===
+FreeIPA %(version)s is a stabilization release for the features delivered as a
+part of 4.4.0. There are more than %(num_bugs)s bug-fixes which details can be seen in
+the list of resolved tickets below.
+
+== Upgrading ==
+Upgrade instructions are available on [[Upgrade]] page.
+
+== Feedback ==
+Please provide comments, bugs and other feedback via the freeipa-users mailing
+list (http://www.redhat.com/mailman/listinfo/freeipa-users) or #freeipa
+channel on Freenode.
+
+"""
+
+
+GIT_DIR = None; assert GIT_DIR, "Specify path to clean git repository here"
+PAGURE_REPO = "freeipa"
+PAGURE_TOKEN = None; assert PAGURE_TOKEN, "Specify pagure token here"
+
+
+class OperationError(Exception):
+    foo = ''
+
+
+def query(pagure, status=None, tags=None, assignee=None, author=None):
+    issues = pagure.list_issues(status, tags, assignee, author)
+    return issues
+
+def normalize_val(val):
+    """Pagure returns values in random casing, normalize to lowercase
+    :param val: input value
+    :return: None or lowercase string
+    """
+    if val is None:
+        return val
+    else:
+        return val.lower()
+
+def filter_by_close_status(tickets, statuses=()):
+    # compare in lower case
+    statuses = [s.lower() for s in statuses]
+    matching = [
+        t for t in tickets
+        if normalize_val(t.get('close_status')) in statuses
+    ]
+    return matching
+
+
+def filter_by_milestones(tickets, milestones=()):
+    # compare in lower case
+    milestones = [m.lower() for m in milestones]
+    matching = [
+        t for t in tickets
+        if normalize_val(t.get('milestone')) in milestones]
+    return matching
+
+
+def get_custom_field(ticket, name, default=None):
+    for field in ticket.get('custom_fields', ()):
+        if field['name'].lower() == name.lower():
+            return field['value']
+    return default
+
+
+class App(object):
+    def __init__(self, args):
+        self.args = args
+
+    def run(self):
+        pagure = Pagure(
+            pagure_repository=PAGURE_REPO,
+            pagure_token=PAGURE_TOKEN
+        )
+        git = self._get_commits()
+
+        # pagure cannot list tickets by Milestone
+        all_closed = query(pagure, "Closed")
+        all_fixed = filter_by_close_status(all_closed, [u'fixed'])
+
+        primary_tickets = filter_by_milestones(all_fixed, self.args.milestone)
+        if self.args.milestones:
+            possibly_fixed = filter_by_milestones(all_fixed, self.args.milestones)
+            fixed_here_tickets = self._filter_tickets(possibly_fixed, git.commits)
+            fixed_here_tickets = primary_tickets + fixed_here_tickets
+        else:
+            fixed_here_tickets = primary_tickets
+
+        bugs = self._get_bugs(fixed_here_tickets)
+        self._print_wiki(fixed_here_tickets, bugs, git)
+
+    def _get_commits(self):
+        git = GitInfo(GIT_DIR)
+        log = git.get_log(self.args.revision_range)
+        git.add_commits(log)
+        return git
+
+    def _get_bugs(self, tickets):
+        bugs = []
+        for ticket in tickets:
+            # bugs migrated from Trac
+            if normalize_val(get_custom_field(ticket, 'type')) == 'defect':
+                bugs.append(ticket)
+            # new pagure which doesn't have type
+            elif 'rfe' not in ticket.get('tags'):
+                bugs.append(ticket)
+        return bugs
+
+
+    def _filter_tickets(self, tickets, commits):
+        commit_tickets = {}
+        filtered = []
+        for commit in commits:
+            for t_id in commit.tickets:
+                commit_tickets[t_id] = commit
+        for ticket in tickets:
+            if commit_tickets.get(str(ticket.get('id'))):
+                filtered.append(ticket)
+
+        return filtered
+
+    def _release_notes(self, tickets):
+        release_notes = []
+        for ticket in tickets:
+            release_note = get_custom_field(ticket, 'changelog')
+            if release_note:
+                release_notes.append(
+                    "* {t[id]}: {t[title]}\n{changes}\n--------".format(
+                        t=ticket, changes=release_note
+                    )
+                )
+        return "\n".join(release_notes)
+
+    def _print_wiki(self, tickets, bugs, git):
+        bugs = len(bugs) - len(bugs) % 10 # get only estimate
+
+        wiki = WIKI_BLOB % dict(
+            version=self.args.version,
+            prev_version=self.args.prev_version,
+            release_date=self.args.release_date,
+            num_bugs=bugs,
+            release_notes=self._release_notes(tickets)
+        )
+        print(wiki)
+        self._print_tickets_wiki(tickets)
+        self._print_commits_wiki(git)
+
+    def _print_tickets_wiki(self, tickets):
+        print("== Resolved tickets ==")
+        for ticket in tickets:
+            num = ticket.get('id')
+            summary = ticket.get('title')
+            if self.args.links:
+                print("* [https://pagure.io/freeipa/issue/%s #%s] %s" %
+                    (num, num, summary))
+            else:
+                print("* %s %s" %(num, summary))
+
+    def _print_commits_wiki(self, git):
+        print("== Detailed changelog since %s ==" % self.args.prev_version)
+        authors = git.authors.keys()
+        authors.sort()
+        for mail in authors:
+            author = git.authors[mail]
+            commits = author.commits
+            if len(commits) < 1:
+                continue
+            print("=== %s (%d) ===" % (author.name, len(commits)))
+            for c in commits:
+               self._print_commit_wiki(c)
+            print("")
+
+    def _print_commit_wiki(self, commit):
+        base = "* %s" % commit.summary.strip()
+        if self.args.links:
+            cgit = "[https://pagure.io/freeipa/c/%s commit]" % (
+                commit.commit)
+            tickets = []
+            for t in sorted(list(commit.tickets)):
+                tickets.append("[https://pagure.io/freeipa/issue/%s #%s]" % (t, t))
+            tickets = ', '.join(tickets)
+            print(base + " " + cgit + " " + tickets)
+        else:
+            print(base)
+
+
+def parse_args():
+    desc = """
+    Release notes \n \n
+
+    python release-notes.py 4.4.2 2016-10-05 4.4.1 4.4.0 release-4-4-1..ipa-4-4 \
+     "FreeIPA 4.4.2" -m  "Freeipa 4.2.4" "FreeIPA 4.2.5"  "FreeIPA 4.3.2" \
+      --links
+    """
+    # create the top-level parser
+    parser = argparse.ArgumentParser(
+        prog='release-notes',
+        description=desc)
+    # parser.add_argument('-v', '--verbose', dest='v', action='count', default=0,
+    #                     help='verbose output')
+
+    parser.add_argument('version',  help='Released version')
+    parser.add_argument('release_date', help='Released date')
+    parser.add_argument('prev_version', help='Previous version')
+    parser.add_argument('major_version', help='Major version')
+    parser.add_argument('revision_range',
+                        help='Revision range as in git log')
+    parser.add_argument('milestone',
+                        help='Primary milestone')
+    parser.add_argument('--milestone', '-m', dest='milestones',
+                        nargs='*', help='Additional milestones')
+    parser.add_argument('--links', action="store_true",
+                        help='With links to tickets and commits')
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+    try:
+        app = App(args)
+        app.run()
+    except OperationError as e:
+        print(e)
+        sys.exit(1)
+    except Exception as e:
+        print('***The command above has FAILED***')
+        traceback.print_exc()
+        sys.exit(1)
