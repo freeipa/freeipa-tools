7764d161aa552bfedd3aecf611cc240a2c94d902
Martin Basti <mbasti@redhat.com>
2017-05-23 18:59:23 +0200

    move_milestone: make script more CLI usable

--- release/move-milestone.py
+++ release/move-milestone.py
@@ -18,15 +18,12 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import print_function
+import argparse
 import time
 from libpagure import Pagure
 
 PAGURE_REPO = "freeipa"
-PAGURE_TOKEN = None; assert PAGURE_TOKEN, "Specify pagure token here"
-MOVE_FROM = "FreeIPA 4.5.1"
-MOVE_TO = "FreeIPA 4.5.2"
-MESSAGE = "{} has been released, moving to {} milestone".format(
-    MOVE_FROM, MOVE_TO)
+MESSAGE = "{move_from} has been released, moving to {move_to} milestone"
 
 
 def opened_tickets(pagure, milestone):
@@ -58,9 +55,67 @@ def move_tickets_to_milestone(pagure, move_from, move_to, message=None):
             pagure.comment_issue(t_num, message)
         time.sleep(5)  # Do not hammer pagure API
 
-if __name__ == '__main__':
+def parse_args():
+    desc = """
+    Migrate opened tickets to a new milestone\n \n
+
+    Example: ./move-milestone "FreeIPA 4.5.1" "FreeIPA 4.5.2" --add-comment
+    """
+    # create the top-level parser
+    parser = argparse.ArgumentParser(
+        prog='move-milestone',
+        description=desc)
+
+    parser.add_argument('move_from', help='Migrate from this mileston')
+    parser.add_argument('move_to', help='Migrate to this milestone')
+    parser.add_argument('--message', dest='message',
+                        help='Comment to be added to tickets, see '
+                             '--add-comment option (allowed substitution '
+                             '{move_from}, {move_to}). Default: "%s"'
+                             % MESSAGE,
+                        default=MESSAGE)
+    parser.add_argument('--add-comment', dest='add_comment',
+                        action="store_true",
+                        help='Add comment to tickets about migrating',
+                        default=False)
+    parser.add_argument('--token', dest='token', action='store',
+                        help='Pagure token for accessing issues',
+                        metavar='TOKEN', default=None)
+    parser.add_argument('--token-file', dest='token_file', action='store',
+                        help='Path to file where pagure token is stored',
+                        metavar='PATH', default=None)
+    args = parser.parse_args()
+
+    if not (args.token or args.token_file):
+        raise RuntimeError(
+            "Please specify --token or --token-file for pagure access")
+    return args
+
+
+def main():
+    args = parse_args()
+
+    pagure_token = None
+    if args.token:
+        pagure_token = args.token
+    elif args.token_file:
+        with open(args.token_file, 'r') as f:
+            pagure_token = f.read().strip()
+    else:
+        RuntimeError("Please specify Pagure token")
+
+    message = None
+    if args.add_comment:
+        message = args.message.format(
+            move_from=args.move_from, move_to=args.move_to)
+
     pagure = Pagure(
         pagure_repository=PAGURE_REPO,
-        pagure_token=PAGURE_TOKEN
+        pagure_token=pagure_token
     )
-    move_tickets_to_milestone(pagure, MOVE_FROM, MOVE_TO, message=MESSAGE)
+
+    move_tickets_to_milestone(
+        pagure, args.move_from, args.move_to, message=message)
+
+if __name__ == '__main__':
+    main()
