bfa970df7e695036938afd25c6201820e8d08520
Michal Reznik <mreznik@redhat.com>
2017-06-27 13:10:29 +0200

    makepki.py - Subject Key Identifier change
    As per RFC5280 subject key identifiers SHOULD be derived from
    the public key or a method that generates unique values.
    
    Currently we use the latter which should not be bad but as
    python-cryptography offers from_public_key() method we can
    use it instead.
    
    Signed-off-by: Michal Reznik <mreznik@redhat.com>

--- makepki.py
+++ makepki.py
@@ -157,8 +157,11 @@ def profile_ca(builder, ca_nick):
         ]),
         critical=False,
     )
+    
+    public_key = builder._public_key
+
     builder = builder.add_extension(
-        x509.SubjectKeyIdentifier(digest=base64.b64encode(os.urandom(64))),
+        x509.SubjectKeyIdentifier.from_public_key(public_key),
         critical=False,
     )
 
